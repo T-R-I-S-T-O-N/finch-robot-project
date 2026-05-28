22d32e6c92d7af9400a3971f693c949e75e6dd24
BMFBigGorilla <thamilto0010@mymail.lausd.net>
2026-05-28 13:19:38 -0700

    yippy yay uhh api update :000
    added an API method

---
+++ src/main/java/com/pandappl/FinalFinch.java
@@ -0,0 +1,16 @@
+package com.pandappl;
+
+import com.birdbrain.Finch;
+
+public class FinalFinch extends Finch {
+    
+public FinalFinch(String Device){
+    super(Device);
+}
+
+
+
+
+
+
+}
---
+++ src/main/java/com/pandappl/TesterFinch.java
@@ -0,0 +1,13 @@
+package com.pandappl;
+
+public class TesterFinch {
+    
+public static void main(String[] args) {
+    FinalFinch bird = new FinalFinch("A");
+    bird.setBeak(60, 50, 70);
+    bird.setMove("B", 20, 50);
+}
+
+
+
+}
