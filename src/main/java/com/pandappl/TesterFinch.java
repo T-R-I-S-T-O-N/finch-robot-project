b32433b8dbfb1811bae9ffe20c8e42a7a8d55b0f
BMFBigGorilla <thamilto0010@mymail.lausd.net>
2026-05-29 13:19:16 -0700

    we can make shapes
    we can make shapes, sounds, and turn

--- src/main/java/com/pandappl/TesterFinch.java
+++ src/main/java/com/pandappl/TesterFinch.java
@@ -4,8 +4,28 @@ public class TesterFinch {
     
 public static void main(String[] args) {
     FinalFinch bird = new FinalFinch("A");
+   bird.playNote(60,1);
     bird.setBeak(60, 50, 70);
-    bird.setMove("B", 20, 50);
+    bird.playNote(61,2);
+    bird.setMove("F", 10, 100);
+    bird.setTurn("L", 115, 100);
+    bird.setMove("F", 15, 100);
+    bird.setTurn("L", 141, 100);
+    bird.setMove("F", 15, 100);
+    
+    
+    bird.playNote(63,3);
+    
+    
+    //bird.setTurn("L", 90, 100);
+    //bird.playNote(64,4);
+   // bird.setMove("B", 5, 100);
+   // bird.setTurn("L", 90, 100);
+   // bird.setMove("B", 5, 100);
+   // bird.setTurn("L", 90, 100);
+   // bird.setMove("B", 5, 100);
+   // bird.setTurn("L", 90, 100);
+   // bird.setBeak(60, 20, 20);
 }
 
 
