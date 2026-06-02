e6bb1dafd6a0c8557f455a2ab088cd189306c4e7
BMFBigGorilla <thamilto0010@mymail.lausd.net>
2026-06-02 12:52:18 -0700

    2nd to last phase

--- src/main/java/com/pandappl/TesterFinch.java
+++ src/main/java/com/pandappl/TesterFinch.java
@@ -7,27 +7,36 @@ public static void main(String[] args) {
    bird.playNote(60,1);
     bird.setBeak(60, 50, 70);
     bird.playNote(61,2);
+  
+//   // make the box
     bird.setMove("F", 10, 100);
     bird.setTurn("L", 90, 100);
     bird.setMove("F", 10, 100);
     bird.setTurn("L", 90, 100);
     bird.setMove("F", 10, 100);
+   
+//    // the top
     bird.setTurn("L", 45, 100);
     bird.setMove("F", 8, 100);
     bird.setTurn("L", 100, 100);
     bird.setMove("F", 8, 100);
-    bird.setTurn("L", 130, 100);
+   
+
+//     // back to bottom right
+    bird.setTurn("L", 129.7, 100);
+    bird.setMove("F", 10.5, 100);
+    bird.setTurn("R", 89.5, 100);
     bird.setMove("F", 10, 100);
-    bird.setTurn("R", 90, 100);
+    
+//     // form the thrusters
+    bird.setTurn("L", 45, 100);
     bird.setMove("F", 10, 100);
-    bird.setTurn("R", 90, 100);
-    bird.setMove("F", 3, 1);
-    bird.setTurn("R", 90, 100);
-    bird.setMove("F", 5, 100);
-    bird.setTurn("L", 90, 100);
-    bird.setMove("F", 2, 100);
-    bird.setTurn("L", 90, 100);
-    bird.setMove("F", 5, 100);
+  
+  
+    bird.setTurn("R", 140, 100);
+//     bird.setMove("F", 8, 100);
+   
+    
       
 
 
@@ -36,15 +45,7 @@ public static void main(String[] args) {
     bird.playNote(63,3);
     
     
-    //bird.setTurn("L", 90, 100);
-    //bird.playNote(64,4);
-   // bird.setMove("B", 5, 100);
-   // bird.setTurn("L", 90, 100);
-   // bird.setMove("B", 5, 100);
-   // bird.setTurn("L", 90, 100);
-   // bird.setMove("B", 5, 100);
-   // bird.setTurn("L", 90, 100);
-   // bird.setBeak(60, 20, 20);
+    
 }
 
 
